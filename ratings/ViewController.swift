11a5874c421cb21f23e049aa121a219609eccccf
Yashovardhan Chaturvedi <yash@yashovardhans-macbook-pro.local>
2016-01-16 14:46:46 +0530

    Initial Commit

---
+++ ratings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ratings
+//
+//  Created by Yashovardhan Chaturvedi on 16/01/16.
+//  Copyright © 2016 YC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
